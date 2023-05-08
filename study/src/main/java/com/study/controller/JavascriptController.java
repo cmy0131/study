d8265455c0defead7e62cd164a08ee236527968b
cmo01 <cmo01@DESKTOP-0DEH118>
2023-05-08 13:17:17 +0900

    day2(javascript,jquery)

---
+++ study/src/main/java/com/study/controller/JavascriptController.java
@@ -0,0 +1,97 @@
+package com.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/javascript")
+public class JavascriptController {
+
+	String dir="javascript/";
+	
+	
+	@RequestMapping("")
+	public String main(Model model) {
+		model.addAttribute("left", dir+"left");
+		model.addAttribute("center", dir+"center");
+		return "main";
+	}
+	
+	@RequestMapping("/ch1")
+	public String ch1(Model model) {
+		model.addAttribute("left", dir+"left");
+		model.addAttribute("center", dir+"ch1");
+		return "main";
+	}
+	
+	@RequestMapping("/ch2")
+	public String ch2(Model model) {
+		model.addAttribute("left", dir+"left");
+		model.addAttribute("center", dir+"ch2");
+		return "main";
+	}
+	
+	@RequestMapping("/ch3")
+	public String ch3(Model model) {
+		model.addAttribute("left", dir+"left");
+		model.addAttribute("center", dir+"ch3");
+		return "main";
+	}
+	
+	@RequestMapping("/ch4")
+	public String ch4(Model model) {
+		model.addAttribute("left", dir+"left");
+		model.addAttribute("center", dir+"ch4");
+		return "main";
+	}
+	
+	@RequestMapping("/ch5")
+	public String ch5(Model model) {
+		model.addAttribute("left", dir+"left");
+		model.addAttribute("center", dir+"ch5");
+		return "main";
+	}
+	
+	@RequestMapping("/ch6")
+	public String ch6(Model model) {
+		model.addAttribute("left", dir+"left");
+		model.addAttribute("center", dir+"ch6");
+		return "main";
+	}
+	
+	@RequestMapping("/ch7")
+	public String ch7(Model model) {
+		model.addAttribute("left", dir+"left");
+		model.addAttribute("center", dir+"ch7");
+		return "main";
+	}
+	
+	@RequestMapping("/ch8")
+	public String ch8(Model model) {
+		model.addAttribute("left", dir+"left");
+		model.addAttribute("center", dir+"ch8");
+		return "main";
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ study/target/classes/static/js/ch1.js
@@ -0,0 +1,5 @@
+
+window.onload = function(){
+	var h1 = document.querySelector('#h1').innerHTML;
+	document.querySelector('#h2').innerHTML = h1;
+}
